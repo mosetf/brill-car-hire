de487481f38b84453a5007e9ccd763d07c810c0a
mosetf <stanlym315@gmail.com>
2023-12-13 22:42:55 +0300

    incooperate the Edit profile form for the users

--- website/forms.py
+++ website/forms.py
@@ -1,19 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, SubmitField, SelectField,FloatField,StringField, PasswordField
+from wtforms import DateField, SubmitField, SelectField, FloatField, StringField, PasswordField
 from wtforms.validators import DataRequired, Email
 
 class BookingForm(FlaskForm):
+    """
+    Form for booking a car.
+    """
     car_id = SelectField('Select Car', coerce=int, validators=[DataRequired()])
     start_date = DateField('Start Date', format='%Y-%m-%d', validators=[DataRequired()])
     end_date = DateField('End Date', format='%Y-%m-%d', validators=[DataRequired()])
     confirm_booking = SubmitField('Confirm Booking')
 
 class CarPriceForm(FlaskForm):
+    """
+    Form for updating car prices.
+    """
     base_price = FloatField('Base Price', validators=[DataRequired()])
-    submit = SubmitField('Update Price')    
+    submit = SubmitField('Update Price')
 
 class EditProfileForm(FlaskForm):
+    """
+    Form for editing user profile information.
+    """
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('New Password')
-    submit = SubmitField('Update Profile')    
+    submit = SubmitField('Update Profile')
+
