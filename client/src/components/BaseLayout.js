1a156182831fe4d24c20d21a67cbbab41d6d315e
mosetf <stanlym315@gmail.com>
2024-07-28 15:29:48 +0300

    feat: Add BaseLayout component
    This commit adds the BaseLayout component to the client/src/components directory. The BaseLayout component is a layout component that provides a common structure and styling for the application. It includes the necessary HTML head elements, a navigation bar, and a container for rendering child components.

---
+++ client/src/components/BaseLayout.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+const BaseLayout = ({ title, children }) => {
+  return (
+    <div>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          crossOrigin="anonymous"
+        />
+        <title>{title}</title>
+        <script>
+          {`function confirmLogout() {
+            return confirm("Are you sure you want to logout?");
+          }`}
+        </script>
+      </head>
+      <body>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Brill Car Hire</a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Home</a>
+                </li>
+                {/* Add conditional rendering for authenticated user */}
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          {children}
+        </div>
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossOrigin="anonymous"
+        ></script>
+      </body>
+    </div>
+  );
+};
+
+export default BaseLayout;
